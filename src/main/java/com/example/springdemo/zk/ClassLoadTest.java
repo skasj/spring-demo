9d94ba134bd9be00873e516a791c3bb4bf4a4bb9
skasj <993248739@qq.com>
2021-07-08 15:30:31 +0800

    [新增功能](1.0): classLoadTest

---
+++ src/main/java/com/example/springdemo/zk/ClassLoadTest.java
@@ -0,0 +1,34 @@
+package com.example.springdemo.zk;
+
+import com.sun.org.apache.xml.internal.resolver.helpers.FileURL;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import org.apache.dubbo.common.utils.StringUtils;
+
+public class ClassLoadTest {
+    static class MyClassLoad extends URLClassLoader {
+
+        public MyClassLoad(URL[] urls) {
+            super(urls);
+        }
+        @Override
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            return super.findClass(name);
+        }
+    }
+
+    public static void main(String[] args)
+        throws MalformedURLException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+
+        URL url = FileURL.makeURL("/Users/xiedongyu/IdeaProjects/spring-demo/src/main/java/java/lang");
+        MyClassLoad myClassLoad = new MyClassLoad(new URL[]{url});
+        Class<?> string = myClassLoad.loadClass("String");
+        Object obj = string.newInstance();
+        Method method = string.getDeclaredMethod("print",null);
+        //通过反射调用Test类的say方法
+        System.out.println(method.invoke(obj, null));
+    }
+}
